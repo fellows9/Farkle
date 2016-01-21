5bb1f15e6eb32c5eb13fd7625e2b54d5c91d5e95
Steven Fellows <stevenfellows@stevens-macbook-pro-2.local>
2016-01-21 14:13:04 -0600

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Steven Fellows on 1/21/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
